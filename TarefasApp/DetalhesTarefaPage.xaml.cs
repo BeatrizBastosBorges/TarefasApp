58db84ed4cb4cdb1d84a41ea096269ad624b4d3b
Beatriz Bastos <biaborges.estudante@gmail.com>
2025-09-30 17:50:01 -0300

    Criação da tela de detalhes da tarefa

---
+++ TarefasApp/DetalhesTarefaPage.xaml.cs
@@ -0,0 +1,36 @@
+namespace TarefasApp;
+
+public partial class DetalhesTarefaPage : ContentPage
+{
+    private Tarefa _tarefa;
+    public DetalhesTarefaPage(Tarefa tarefa)
+    {
+        InitializeComponent();
+        _tarefa = tarefa;
+
+        tituloLabel.Text = _tarefa.Titulo;
+        descricaoLabel.Text = _tarefa.Descricao;
+        dataLabel.Text = _tarefa.DataCriacao.ToShortDateString();
+        prioridadeLabel.Text = _tarefa.Prioridade.ToString();
+    }
+
+    private async void OnEditarClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AdicionarEditarTarefaPage(_tarefa));
+    }
+
+    private async void OnExcluirClicked(object sender, EventArgs e)
+    {
+        bool confirmado = await DisplayAlert(
+            "Confirmar Exclusão",
+            $"Você tem certeza que deseja excluir a tarefa '{_tarefa.Titulo}'?",
+            "Sim",
+            "Não");
+
+        if (confirmado)
+        {
+            TarefaRepository.RemoverTarefa(_tarefa.Id);
+            await Navigation.PopAsync();
+        }
+    }
+}
